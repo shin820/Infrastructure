990b28e28a6cf8029c4d3777024690bc76cf8457
shin820 <shin820@163.com>
2014-07-28 18:04:57 +0800

    add MyIdentity.cs

---
+++ Infrastructure.Common/Authentication/MyIdentity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common.Authentication
+{
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
+    public class MyIdentity : DataCollection<string, object, StringComparer>, IIdentity
+    {
+        private MyIdentity(IIdentity identity, AuthCookieTicket authTicket)
+            : base(StringComparer.OrdinalIgnoreCase)
+        {
+            this.Name = identity.Name;
+            this.AuthenticationType = identity.AuthenticationType;
+            this.IsAuthenticated = identity.IsAuthenticated;
+
+            this.Initalize(authTicket);
+        }
+
+        private void Initalize(AuthCookieTicket authTicket)
+        {
+            foreach (var entry in authTicket)
+            {
+                this.Add(entry.Key, entry.Value);
+            }
+        }
+
+        public string Name { get; private set; }
+        public string AuthenticationType { get; private set; }
+        public bool IsAuthenticated { get; private set; }
+    }
+}
