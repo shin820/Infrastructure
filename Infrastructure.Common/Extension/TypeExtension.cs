70e6634d88bc971d3da41fd8d75082b6d0ce6ec6
shin820 <shin820@163.com>
2014-08-01 10:54:44 +0800

    add TypeExtension.cs

---
+++ Infrastructure.Common/Extension/TypeExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Infrastructure.Common.Extension
+{
+    /// <summary>
+    /// Type扩展
+    /// </summary>
+    public static class TypeExtension
+    {
+        /// <summary>
+        /// 判断子类是否可以赋值给基类，支持开放泛型
+        /// </summary>
+        /// <param name="extendType">子类型</param>
+        /// <param name="baseType">基类型</param>
+        /// <returns>是否可以赋值</returns>
+        public static bool IsAssignableTo(this Type extendType, Type baseType)
+        {
+            while (!baseType.IsAssignableFrom(extendType))
+            {
+                if (extendType == null)
+                {
+                    return false;
+                }
+
+                if (extendType == typeof(object))
+                {
+                    return false;
+                }
+
+                if (extendType.IsGenericType && !extendType.IsGenericTypeDefinition)
+                {
+                    extendType = extendType.GetGenericTypeDefinition();
+                }
+                else
+                {
+                    extendType = extendType.BaseType;
+                }
+            }
+
+            return true;
+        }
+    }
+}
